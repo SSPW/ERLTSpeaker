9281db41dfdea388ffc766557562d15123528f29
John Craven <craven@Diskstation>
2016-04-12 16:38:12 -0400

    Change configuration filename

--- iOS/ERLTSpeaker.py
+++ iOS/ERLTSpeaker.py
@@ -1,3 +1,13 @@
+# EasyRaceLapTimer Pythonista Client with Speech
+# Version for iOS Pythonista
+#
+# Author: John Craven
+#
+# This is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+# This software is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License along with Foobar. If not, see http://www.gnu.org/licenses/.
+
+
 # coding: utf-8
 from time import sleep
 import requests
@@ -16,7 +26,7 @@ def replacevalue(m, pattern, replacement):
 	return m.replace(pattern, '{}'.format(replacement))
 	
 try:
-	f1 = open('ERLTconfig.csv','r')
+	f1 = open('ERLTSpeaker.cfg','r')
 	token = readline_stripped(f1)
 	ip = readline_stripped(f1)
 	interval = readline_stripped(f1)
@@ -51,7 +61,7 @@ if (form != None):
 	best_time_phrase = form.get('besttimephrase')
 	message_format = form.get('messageformat')
 
-	f2 = open('ERLTconfig.csv','w')
+	f2 = open('ERLTSpeaker.cfg','w')
 	writevalueline(f2, token)
 	writevalueline(f2, ip)
 	writevalueline(f2, interval)
